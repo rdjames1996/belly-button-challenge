0b28faa90733e65e6fd15db6f1392eec860163a3
rdjames1996 <robjames@Roberts-MacBook-Air.local>
2023-03-16 14:05:05 -0700

    test 1

---
+++ static/js/app.js
@@ -0,0 +1,75 @@
+function dropdown(){
+    d3.json("./samples.json").then(data => {
+        var sampleName = data.names;
+        var dataid = d3.select("#selDataset")
+        sampleName.forEach((sampleid)=> {
+            dataid.append("option")
+                .text(sampleid)
+                .property("value", sampleid)
+        })
+        var firstSample = sampleName[0];
+        metadata(firstSample);
+        Charts(firstSample);
+    // console.log(sampleId)
+    });
+};
+
+dropdown();
+
+function metadata(id) {
+    d3.json("./samples.json").then(data => {
+        var sampleMetadata = data.metadata;
+        var result = sampleMetadata.filter(obj => obj.id == id);
+        var filterresult = result[0];
+        var display = d3.select("#sample-metadata");
+        display.html("");
+        Object.entries(filterresult).forEach(([key, value]) => {
+            display.append("h6").text(`${key} ${value}`);
+        });
+    });
+}
+
+// Use sample_values as the values for the bar chart.
+// Use otu_ids as the labels for the bar chart.
+// Use otu_labels as the hovertext for the chart.
+
+function Charts(id){
+    d3.json("./samples.json").then(data => {
+        var sampledata = data.samples;
+        var result = sampledata.filter(obj => obj.id == id);
+        var filterresult = result[0];
+        var otu_ids = filterresult.otu_ids;
+        var otu_labels = filterresult.otu_labels;
+        var sample_values = filterresult.sample_values;
+
+        var bubbledata = [{
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Picnic"
+            }
+        }];
+        
+        var barData = [{
+            y: otu_ids.slice(0, 10).map(val => `OTU ${val}`).reverse(),
+            x: sample_values.slice(0, 10).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h",
+            border: 2
+        }];
+
+        Plotly.newPlot("bar", barData);
+
+        Plotly.newPlot("bubble", bubbledata);
+    });
+};
+
+function optionChanged(changeid) {
+    metadata(changeid);
+    Charts(changeid);
+};
